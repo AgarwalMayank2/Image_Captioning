32c6c249b50f833ff539a5908f842a41c670b7ad
AgarwalMayank2 <mayankagarwal020905@gmail.com>
2025-05-27 01:52:45 +0530

    encoder

---
+++ resnet_encoder.py
@@ -0,0 +1,50 @@
+from torchvision import transforms
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from PIL import Image
+from torchvision.models import resnet50
+from torch.autograd import Variable
+import torch
+
+class extractImageFeature:
+    def __init__(self, data):
+        self.data = data
+        self.transforms = transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            transforms.ToTensor()
+        ])
+    
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        image_name = self.data.iloc[idx]['image']
+        image_loc = 'flickr8k/Images/'+image_name
+
+        img = Image.open(image_loc)
+        transformed_img = self.transforms(img)
+
+        return image_name, transformed_img
+
+
+def extract_features(data, batch_size = 1):
+    image_dataset = extractImageFeature(data)
+    image_dataloader = DataLoader(image_dataset, batch_size = batch_size, shuffle = False)
+
+
+class encode:
+    def __init__(self, device):
+        self.device = device
+        self.resnet = resnet50(pretrained = True).to(device)
+        self.resnet_layer4 = self.resnet._modules.get('layer4').to(device)
+
+    def get_vector(self, image):
+        image = Variable(image)
+        my_embedding = torch.zeros(1, 512, 7, 7)
+        def copy_data(m, i, o):
+            my_embedding.copy_(o.data)
+        h = self.resnet_layer4.register_forward_hook(copy_data)
+        self.resnet(image)
+        return my_embedding
+    
